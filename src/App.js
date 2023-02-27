5b34a836d9de7152eabb637fd8c7699941debfe5
myankpraksh <myankpraksh@gmail.com>
2023-02-27 13:50:22 +0530

    App component converted to rce

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import HeroSection from './components/HeroSection/HeroSection';
+import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <HeroSection/> {/* component contains hero section with a CTA button */}
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeroSection />{" "}
+        {/* component contains hero section with a CTA button */}
+      </div>
+    );
+  }
 }
 
 export default App;
