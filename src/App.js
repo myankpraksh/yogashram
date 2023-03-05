536b9d4efac152c78e092aced27342e45ab700c0
myankpraksh <myankpraksh@gmail.com>
2023-03-05 21:40:35 +0530

    HeroSection added, css variable and state variables added to store current height and width of window. events added to update them

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import "./App.css";
 import HeroSection from "./components/HeroSection/HeroSection";
 import React, { Component } from "react";
-
+import "./App.css";
 export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentWindowHeight: window.innerHeight,
+      currentWindowWidth: window.innerWidth,
+    };
+  };
+  //function to update current window height and width in css variable in App class state variables
+  updateCurrentHeight = () => {
+    document
+      .getElementById("root")
+      .style.setProperty("--currentHeight", `${window.innerHeight}px`);
+    document
+      .getElementById("root")
+      .style.setProperty("--currentWidth", `${window.innerWidth}px`);
+    this.setState({
+      currentWindowHeight: window.innerHeight,
+      currentWindowWidth: window.innerWidth,
+    });
+  };
+  componentDidMount() {
+    //adding event to trigger function to update current window height and width, when components mounts
+    window.addEventListener("resize", this.handleResize);
+  }
+  componentWillUnmount() {
+    //removing event to trigger function to update current window height and width, when components unmounts
+    window.removeEventListener("resize", this.handleResize);
+  }
   render() {
     return (
       <div className="App">
--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,12 +1,15 @@
 // this component contains hero section with a CTA button
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./HeroSection.css";
 
 export class HeroSection extends Component {
   render() {
     return (
-      <div>HeroSection</div>
-    )
+      <div className="HeroSection" id="HeroSection">
+        HeroSection
+      </div>
+    );
   }
 }
 
-export default HeroSection
+export default HeroSection;
