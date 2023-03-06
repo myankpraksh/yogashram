b209b147967af621a0173092dfd9aba30b8e84de
myankpraksh <myankpraksh@gmail.com>
2023-03-07 03:03:43 +0530

    Hero Section background done

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,12 +1,25 @@
-// this component contains hero section with a CTA button
+// this component contains hero section
 import React, { Component } from "react";
+import mandalaImage from "./assets/images/mandala.svg";
+import cloud from "./assets/images/cloud-original.png";
+import lady from "./assets/images/lady.png";
+import cloudBase from "./assets/images/cloud-base.png";
+import cloudCopy from "./assets/images/cloudCopy.png";
 import "./HeroSection.css";
 
 export class HeroSection extends Component {
   render() {
     return (
-      <div className="HeroSection" id="HeroSection">
-        HeroSection
+      <div className="HeroSection">
+        <div className="HeroSectionBackground" id="HeroSectionBackground">
+          <img src={mandalaImage} alt="mandala-artwork" id="mandala-artwork" />
+        </div>
+        <div className="HeroSectionImages">
+          <img src={lady} alt="Lady doing yoga" id="lady" />
+          <img src={cloudBase} alt="Peaceful cloud" id="cloudBase" />
+          <img src={cloud} alt="Peaceful cloud" id="cloud" />
+          <img src={cloudCopy} alt="Peaceful cloud" id="cloudCopy" />
+        </div>
       </div>
     );
   }
