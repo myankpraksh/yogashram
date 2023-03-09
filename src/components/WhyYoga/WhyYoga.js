4f5c9787ebb8caddef19f90bbace94c7b6ff3ef3
myankpraksh <myankpraksh@gmail.com>
2023-03-09 23:49:54 +0530

    added second section WhyYoga

--- src/components/WhyYoga/WhyYoga.js
+++ src/components/WhyYoga/WhyYoga.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react'
-import './WhyYoga.css';
-
+import React, { Component } from "react";
+import "./WhyYoga.css";
+import harmony from "./assets/images/harmony.svg";
+import innerPeace from "./assets/images/innerPeace.svg";
+import strength from "./assets/images/strength.svg";
+import halfLogo from "./assets/images/halfLogo.svg";
 export default class WhyYoga extends Component {
   render() {
     return (
-      <div>WhyYoga</div>
-    )
+      <div className="WhyYoga">
+        <p>Find your inner strength, peace and freedom with Yoga</p>
+        <p id="WhyYogaTitle">Why Yoga?</p>
+        <div className="WhyYogaFlex">
+          <img src={halfLogo} alt="decorative logo" class="halfLogo" />
+          <img src={innerPeace} alt="inner peace" className="cardImage" />
+          <img src={strength} alt="strength" className="cardImage" />
+          <img src={harmony} alt="harmony" className="cardImage" />
+        </div>
+      </div>
+    );
   }
 }
