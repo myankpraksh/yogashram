4331f7c6cde9295a1c48318f18a71eec95410efc
myankpraksh <myankpraksh@gmail.com>
2023-03-10 23:33:21 +0530

    component created for third section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import "./App.css";
 import CreditFooter from "./components/CreditFooter/CreditFooter";
 import WhyYoga from "./components/WhyYoga/WhyYoga";
+import WhyUs from "./components/WhyUS/WhyUs";
 export class App extends Component {
   constructor() {
     super();
@@ -38,6 +39,7 @@ export class App extends Component {
       <div className="App">
         <HeroSection /> {/* component contains hero section with a CTA button */}
         <WhyYoga /> {/*component contains why yoga section} */}
+        <WhyUs /> {/*Component for third section, WhyUs*/}
         <CreditFooter /> {/* component contains credit to designer and developer */}
       </div>
     );
---
+++ src/components/WhyUS/WhyUs.js
@@ -0,0 +1,14 @@
+import "./WhyUs.css";
+import React, { Component } from "react";
+
+export default class WhyUs extends Component {
+  render() {
+    return (
+      <div>
+        <p>Experience like never before, most exclusive studio of the nation</p>
+        <p>Why our Yogasharam</p>
+        <p>Yoga Studio?</p>
+      </div>
+    );
+  }
+}
